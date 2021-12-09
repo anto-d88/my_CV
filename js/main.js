9033065a425c5b425d6e49710eb42e219d36dfa6
anto-88 <anto-88@pop-os.localdomain>
2021-12-09 14:38:32 +0100

    add transition js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// MENU DEROULANT //****************************
 window.addEventListener('scroll', function(){
     const header =document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0 );
@@ -5,8 +6,24 @@ window.addEventListener('scroll', function(){
 
 
 function toggleMenu(){
-const tmenuToggle = document.querySelector('.menuToggle');
+const menuToggle = document.querySelector('.menuToggle');
 const navbar = document.querySelector('.navbar');
 navbar.classList.toggle('active');
 menuToggle.classList.toggle('active');
 }
+
+// CHANGE DE BANNIERE //*****************************
+const changeBanniere = document.querySelector(".contenu");
+
+function interf() {
+    changeBanniere.style.display = "flex";
+};
+
+setTimeout(interf, 1000);
+
+const icone = document.querySelector(".icone");
+
+function iconn() {
+    icone.style.display = "grid";
+};
+setTimeout(iconn, 1500);
